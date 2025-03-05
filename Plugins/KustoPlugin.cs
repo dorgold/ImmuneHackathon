6130a67936e93204959f446a2c47390fead86b35
dorgold <dorgold@gmail.com>
2025-03-05 13:43:29 +0200

    a

--- Plugins/KustoPlugin.cs
+++ Plugins/KustoPlugin.cs
@@ -11,6 +11,50 @@ namespace ImmuneGettingStartedSemanticKernel.Plugins;
 
 public class KustoPlugin
 {
+    private string letMyTable = """
+                                let tenant = "1c48f0fc-5645-4f49-b0a3-b0833a58680a";
+                                let MyTable = cluster('https://eusorncmbesekustodev1.eastus.kusto.windows.net').database('CloudMapV2').GetNodesV2AllTenantsAllScopes
+                                | where TenantId == tenant
+                                | where Label == "device"
+                                | where EntityIds has_cs 'DeviceInventoryId'
+                                | mv-expand EntityId=EntityIds
+                                | where EntityId.type == 'DeviceInventoryId'
+                                | extend DeviceId = tostring(EntityId.id)
+                                | extend DeviceName = Name
+                                | extend DeviceCategory = iff(isempty(tostring(Properties.deviceCategory)), "Unknown", tostring(Properties.deviceCategory))
+                                | extend DeviceType = iff(isempty(tostring(Properties.deviceType)), "Unknown", tostring(Properties.deviceType))
+                                | extend ExposureScore = iff(isempty(tostring(Properties.exposureScore)), "None", tostring(Properties.exposureScore))
+                                | extend OsDistribution = iff(isempty(tostring(Properties.osDistribution)), "Unknown", tostring(Properties.osDistribution))
+                                | extend OsPlatformFriendlyName = iff(isempty(tostring(Properties.osPlatformFriendlyName)), "Unknown", tostring(Properties.osPlatformFriendlyName))
+                                | extend OsBuildRevision = iff(isempty(tostring(Properties.osBuildRevision)), "Unknown", tostring(Properties.osBuildRevision))
+                                | extend OsBuildRevisionBins = case(
+                                OsBuildRevision == "Unknown", "Unknown",
+                                toint(OsBuildRevision) >= 1 and toint(OsBuildRevision) <= 5000, "1-5000",
+                                toint(OsBuildRevision) > 5000 and toint(OsBuildRevision) <= 10000, "5000-10000",
+                                toint(OsBuildRevision) > 10000, ">10000",
+                                "Unknown" 
+                                )
+                                | extend HasTpmData = isnotempty(Properties.tpmData)
+                                | extend OsVersion = iff(isempty(tostring(Properties.osVersion)), "Unknown", tostring(Properties.osVersion))
+                                | extend HasrdpStatus = isnotempty(Properties.rdpStatus)
+                                | extend HasremoteServices = isnotempty(Properties.remoteServicesInfo)
+                                | extend OsArchitecture = iff(isempty(tostring(Properties.osArchitecture)), "Unknown", tostring(Properties.osArchitecture))
+                                | extend OsBuild = iff(isempty(tostring(Properties.osBuild)), "Unknown", tostring(Properties.osBuild))
+                                | extend OsBuildBins = case(
+                                OsBuild == "Unknown", "Unknown",
+                                toint(OsBuild) >= 1 and toint(OsBuild) <= 5000, "1-5000",
+                                toint(OsBuild) > 5000 and toint(OsBuild) <= 10000, "5000-10000",
+                                toint(OsBuild) > 10000, ">10000",
+                                "Unknown" 
+                                )
+                                | extend DeviceDynamicTags = tostring(Properties.deviceDynamicTags)
+                                | where OsDistribution == 'Windows'
+                                | where DeviceCategory == 'Endpoint'
+                                | where DeviceType in ('Workstation', 'Server')
+                                | where OsPlatformFriendlyName startswith 'Windows'
+                                | project TenantId, Id, DeviceId, DeviceName, DeviceType, ExposureScore, OsPlatformFriendlyName, OsBuildRevisionBins, HasTpmData, OsVersion, HasrdpStatus, HasremoteServices, OsArchitecture, OsBuildBins, DeviceDynamicTags;
+                                """;
+
     public KustoPlugin()
     {
         var clusterUri = "https://immunequerystg.eastus2.kusto.windows.net/";
@@ -32,14 +76,90 @@ public class KustoPlugin
     //     return stringWriter.ToString()!;
     // }
 
-    [KernelFunction, Description("Run aribtrary KQL queries on MyTable, returns a CSV of the results or an error string")]
+    [KernelFunction("Query"), Description("Run aribtrary KQL queries on MyTable, returns a CSV of the results or an error string")]
     [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1024:Use properties where appropriate", Justification = "Too smart")]
     public string RunQuery(
         [Description("The KQL query to run.")]
         string query)
     {
-        query = query.Replace("MyTable", "materialized_view('LatestDeviceInfoProfilesView')");
+        // query = query.Replace("MyTable", "materialized_view('LatestDeviceInfoProfilesView') | where OrgId == 'd650c28a-47bd-491c-8426-42847b4c865b'");
         Console.WriteLine($"Running query: {query}");
+        query = letMyTable + query + " | take 5000";
+        try
+        {
+            var response = KustoClient.ExecuteQuery("PhoenixData", query, null);
+            using TextWriter stringWriter = new StringWriter();
+            response.WriteAsCsv(true, stringWriter);
+            return stringWriter.ToString()!;
+        }
+        catch (Exception e)
+        {
+            return e.Message;
+        }
+    }
+
+    [KernelFunction("Score"), Description("Score gets a comma separated list of unique DeviceIds and return a score for the uniquness of the set. The score is a value between 0 and 1")]
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1024:Use properties where appropriate", Justification = "Too smart")]
+    public string Score(
+        [Description("Comma sepparated list of unique DeviceIds.")]
+        List<string> DeviceIds)
+    {
+        var query = $"""
+                     let tenant = "1c48f0fc-5645-4f49-b0a3-b0833a58680a";
+                     let EnvDevices = cluster('https://eusorncmbesekustodev1.eastus.kusto.windows.net').database('CloudMapV2').GetNodesV2AllTenantsAllScopes
+                     | where TenantId == tenant
+                     | where Label == "device"
+                     | where EntityIds has_cs 'DeviceInventoryId'
+                     | mv-expand EntityId=EntityIds
+                     | where EntityId.type == 'DeviceInventoryId'
+                     | extend DeviceId = tostring(EntityId.id)
+                     | extend DeviceName = Name
+                     | extend DeviceCategory = iff(isempty(tostring(Properties.deviceCategory)), "Unknown", tostring(Properties.deviceCategory))
+                     | extend DeviceType = iff(isempty(tostring(Properties.deviceType)), "Unknown", tostring(Properties.deviceType))
+                     | extend ExposureScore = iff(isempty(tostring(Properties.exposureScore)), "None", tostring(Properties.exposureScore))
+                     | extend OsDistribution = iff(isempty(tostring(Properties.osDistribution)), "Unknown", tostring(Properties.osDistribution))
+                     | extend OsPlatformFriendlyName = iff(isempty(tostring(Properties.osPlatformFriendlyName)), "Unknown", tostring(Properties.osPlatformFriendlyName))
+                     | extend OsBuildRevision = iff(isempty(tostring(Properties.osBuildRevision)), "Unknown", tostring(Properties.osBuildRevision))
+                     | extend OsBuildRevisionBins = case(
+                        OsBuildRevision == "Unknown", "Unknown",
+                        toint(OsBuildRevision) >= 1 and toint(OsBuildRevision) <= 5000, "1-5000",
+                        toint(OsBuildRevision) > 5000 and toint(OsBuildRevision) <= 10000, "5000-10000",
+                        toint(OsBuildRevision) > 10000, ">10000",
+                        "Unknown" 
+                     )
+                     | extend HasTpmData = isnotempty(Properties.tpmData)
+                     | extend OsVersion = iff(isempty(tostring(Properties.osVersion)), "Unknown", tostring(Properties.osVersion))
+                     | extend HasrdpStatus = isnotempty(Properties.rdpStatus)
+                     | extend HasremoteServices = isnotempty(Properties.remoteServicesInfo)
+                     | extend OsArchitecture = iff(isempty(tostring(Properties.osArchitecture)), "Unknown", tostring(Properties.osArchitecture))
+                     | extend OsBuild = iff(isempty(tostring(Properties.osBuild)), "Unknown", tostring(Properties.osBuild))
+                     | extend OsBuildBins = case(
+                        OsBuild == "Unknown", "Unknown",
+                        toint(OsBuild) >= 1 and toint(OsBuild) <= 5000, "1-5000",
+                        toint(OsBuild) > 5000 and toint(OsBuild) <= 10000, "5000-10000",
+                        toint(OsBuild) > 10000, ">10000",
+                        "Unknown" 
+                     )
+                     | extend DeviceDynamicTags = tostring(Properties.deviceDynamicTags)
+                     | where OsDistribution == 'Windows'
+                     | where DeviceCategory == 'Endpoint'
+                     | where DeviceType in ('Workstation', 'Server')
+                     | where OsPlatformFriendlyName startswith 'Windows'
+                     | distinct TenantId, Id, DeviceId, DeviceName, DeviceType, ExposureScore, OsPlatformFriendlyName, OsBuildRevisionBins, HasTpmData, OsVersion, HasrdpStatus, HasremoteServices, OsArchitecture, OsBuildBins, DeviceDynamicTags;
+                     let SampleDeviceGroups = EnvDevices;
+                     let devicesCount = toscalar(EnvDevices | count);
+                     // Calculate the representative groups and their weighted counts
+                     let RepresentativeGroups = EnvDevices
+                     | summarize RepresentativeGroupsCount = count() by DeviceType, ExposureScore, OsPlatformFriendlyName, OsBuildRevisionBins, HasTpmData, OsVersion, HasrdpStatus, HasremoteServices, OsArchitecture, OsBuildBins
+                     | extend WeightedCount = RepresentativeGroupsCount * 1.0 / devicesCount;
+                     // Calculate the accumulated sum for the representativeScore groups of chosen machines
+                     let RepresentativeScore = SampleDeviceGroups
+                     | distinct DeviceType, ExposureScore, OsPlatformFriendlyName, OsBuildRevisionBins, HasTpmData, OsVersion, HasrdpStatus, HasremoteServices, OsArchitecture, OsBuildBins
+                     | join kind=inner (RepresentativeGroups) on DeviceType, ExposureScore, OsPlatformFriendlyName, OsBuildRevisionBins, HasTpmData, OsVersion, HasrdpStatus, HasremoteServices, OsArchitecture, OsBuildBins
+                     | summarize RepresentativeScore = round(sum(WeightedCount), 2);
+                     RepresentativeScore
+                     """;
+        Console.WriteLine($"Running Score");
         try
         {
             var response = KustoClient.ExecuteQuery("PhoenixData", query, null);
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Agents;
+using Microsoft.SemanticKernel.Agents.Chat;
 using Microsoft.SemanticKernel.ChatCompletion;
 using Microsoft.SemanticKernel.Connectors.OpenAI;
 using SemanticKernelExamples;
@@ -18,35 +19,118 @@ class Program
         // Kusto Agent
         var kernel = CreateKernel();
         kernel.Plugins.AddFromType<KustoPlugin>("KustoPlugin");
+        KernelFunction scoreFunc = kernel.Plugins.GetFunction("KustoPlugin", "Score");
+        KernelFunction queryFunc = kernel.Plugins.GetFunction("KustoPlugin", "Query");
 
-        var history = new ChatHistory();
+        ChatCompletionAgent dataAnalyst =
+            new()
+            {
+                Instructions = """
+                               You are an are a data analyst, you job is to formulate a strategy for querying the data that KQLMaster will execute and Validator will validate.
+                               """,
+                Name = "DataAnalyst",
+                Kernel = kernel,
+            };
 
         ChatCompletionAgent agent =
             new()
             {
-                Instructions = "You are an expert in KQL (Kusto Query Language).",
-                Name = "KQLmaster",
+                Instructions = """
+                               You are an expert in KQL (Kusto Query Language). You should run multiple queries to get the best results. You always return a list of comma separated DeviceIds or an error.
+                               """,
+                Name = "KQLMaster",
                 Kernel = kernel,
-                Arguments = new KernelArguments(new OpenAIPromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Auto() }),
+                Arguments = new KernelArguments(new OpenAIPromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Required([queryFunc], true, new FunctionChoiceBehaviorOptions() {AllowConcurrentInvocation = true, AllowParallelCalls = true}) }),
             };
 
-        // Respond to user input, invoking functions where appropriate.
-        await InvokeAgentAsync("Choose 10 different DeviceIds from MyTable that have different OS versions. Run multiple queries if needed");
-
-        // Local function to invoke agent and display the conversation messages.
-        async Task InvokeAgentAsync(string input)
-        {
-            ChatMessageContent message = new(AuthorRole.User, input);
-            history.Add(message);
-            WriteAgentChatMessage(message);
-
-            await foreach (ChatMessageContent response in agent.InvokeAsync(history))
+        ChatCompletionAgent checker =
+            new()
             {
-                history.Add(response);
+                Instructions = """
+                               Your job is to check the KQLMaster response (by running a the score function), help him maximize the score. When the score is above 0.8, say it is approved and yield the score and the list of DeviceIds.
+                               If KQLMaster didn't output a list of DeviceIds, say that the response is invalid.
+                               """,
+                Name = "Validator",
+                Kernel = kernel,
+                Arguments = new KernelArguments(new OpenAIPromptExecutionSettings() { FunctionChoiceBehavior = FunctionChoiceBehavior.Required([scoreFunc]) }),
+            };
 
-                WriteAgentChatMessage(response);
-            }
+        KernelFunction terminationFunction =
+            AgentGroupChat.CreatePromptFunctionForStrategy(
+                """
+                Determine if the result has been approved.  If so, respond with a single word: yes
+
+                History:
+                {{$history}}
+                """,
+                safeParameterNames: "history");
+
+//         KernelFunction selectionFunction =
+//             AgentGroupChat.CreatePromptFunctionForStrategy(
+//                 $$$"""
+//                    Determine which participant takes the next turn in a conversation based on the the most recent participant.
+//                    State only the name of the participant to take the next turn.
+//                    No participant should take more than one turn in a row.
+//
+//                    Choose only from these participants:
+//                    - {{{ReviewerName}}}
+//                    - {{{CopyWriterName}}}
+//
+//                    Always follow these rules when selecting the next participant:
+//                    - After {{{CopyWriterName}}}, it is {{{ReviewerName}}}'s turn.
+//                    - After {{{ReviewerName}}}, it is {{{CopyWriterName}}}'s turn.
+//
+//                    History:
+//                    {{$history}}
+//                    """,
+//                 safeParameterNames: "history");
+
+                // Limit history used for selection and termination to the most recent message.
+        ChatHistoryTruncationReducer strategyReducer = new(1);
+
+        // Create a chat for agent interaction.
+        AgentGroupChat chat =
+            new(dataAnalyst, agent, checker)
+            {
+                ExecutionSettings =
+                    new()
+                    {
+                        // Here KernelFunctionTerminationStrategy will terminate
+                        // when the art-director has given their approval.
+                        TerminationStrategy =
+                            new KernelFunctionTerminationStrategy(terminationFunction, kernel)
+                            {
+                                // Only the art-director may approve.
+                                Agents = [checker],
+                                // Customer result parser to determine if the response is "yes"
+                                ResultParser = (result) => result.GetValue<string>()?.Contains("yes", StringComparison.OrdinalIgnoreCase) ?? false,
+                                // The prompt variable name for the history argument.
+                                HistoryVariableName = "history",
+                                // Limit total number of turns
+                                MaximumIterations = 10,
+                                // Save tokens by not including the entire history in the prompt
+                                HistoryReducer = strategyReducer,
+                            },
+                    }
+            };
+
+        // Invoke chat and display messages.
+        ChatMessageContent message = new(AuthorRole.User,
+            """
+                   MyTable has the following columns: DeviceId, DeviceType, ExposureScore, OsPlatformFriendlyName, HasTpmData, OsVersion.
+                   Choose 10 different DeviceIds from MyTable, the objective is to maximize uniqueness. We want the devices to be as diverse as possible.
+                   Look at the the following columns: `DeviceType, ExposureScore, OsPlatformFriendlyName, HasTpmData, OsVersion` to determine diverseness.
+                   Run multiple queries if needed return results in as a list of DeviceIds");
+                   """);
+        chat.AddChatMessage(message);
+        WriteAgentChatMessage(message);
+
+        await foreach (ChatMessageContent responese in chat.InvokeAsync())
+        {
+            WriteAgentChatMessage(responese);
         }
+
+        Console.WriteLine($"\n[IS COMPLETED: {chat.IsComplete}]");
     }
 
     private static Kernel CreateKernel()
